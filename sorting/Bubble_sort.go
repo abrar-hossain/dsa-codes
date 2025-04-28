44f6a3edc1a321d2f94b0e4f3eee47fa5f0e6774
Abrar hossain <abrarmashuk@gmail.com>
2025-04-28 16:24:51 +0100

    Update Bubble_sort.go
    bubble sort

--- sorting/Bubble_sort.go
+++ sorting/Bubble_sort.go
@@ -1,1 +1,19 @@
 
+package main
+
+import "fmt"
+
+func bubbleSort(nums []int) []int {
+	n := len(nums)
+	for i := n - 1; i >= 0; i-- {
+		for j := 0; j <= i-1; j++ {
+			if nums[j] > nums[j+1] {
+				// swap nums[j] and nums[j+1]
+				temp := nums[j+1]
+				nums[j+1] = nums[j]
+				nums[j] = temp
+			}
+		}
+	}
+	return nums
+}
